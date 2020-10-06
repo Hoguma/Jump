623930a5265dd2fcff901a3990ab42377010dd8f
Hoguma <hosung027899@gmail.com>
2020-10-06 19:21:20 +0900

    플랫폼 임시 수정

---
+++ Assets/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private GameObject target;
+    private Collider2D coll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+        target = FindObjectOfType<Player>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.transform.position.y - (0.6371382) > transform.position.y)
+        {
+            coll.enabled = true;
+        }
+        else
+            coll.enabled = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        coll.enabled = true;
+    }
+}
