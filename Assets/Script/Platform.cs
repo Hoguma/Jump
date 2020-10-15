b1801ae294e26850778562797f94b32d1e13b1db
sojunyoup <thwnsuq46@naver.com>
2020-10-15 17:46:01 +0900

    스테이지 벽바꾸기

--- Assets/Script/Platform.cs
+++ Assets/Script/Platform.cs
@@ -1,27 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using System.Collections;
 
 public class Platform : MonoBehaviour
 {
     private GameObject target;
     private Collider2D coll;
+
+    //이미지 변경 1,
+    public Sprite Onestage;
+    private SpriteRenderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
         coll = GetComponent<Collider2D>();
         target = FindObjectOfType<Player>().gameObject;
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (target.transform.position.y - (0.6371382 * 0.65) > transform.position.y)
         {
             coll.enabled = true;
         }
         else
             coll.enabled = false;
+
+        //1단계 점수 로 땅 바꾸기
+        if (GameObject.Find("GameManager").GetComponent<GameManager>().FScore1 >= 20)
+        {
+            myRenderer.sprite = Onestage;
+        }
     }
 
 }
---
+++ Assets/Script/Wallchange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallchange : MonoBehaviour
+{
+    //이미지 변경 1,
+    public Sprite Onestage;
+    private SpriteRenderer myRenderer;
+    void Start()
+    {
+        myRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("GameManager").GetComponent<GameManager>().FScore1 >= 20)
+        {
+            myRenderer.sprite = Onestage;
+        }
+    }
+}
