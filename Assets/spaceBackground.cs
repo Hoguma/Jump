7e257b8eafa31f0042419b750c85ee407ecc700d
sojunyoup <thwnsuq46@naver.com>
2020-12-22 16:57:46 +0900

    spacescroll

---
+++ Assets/spaceBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceBackground : MonoBehaviour
+{
+    public GameObject Up;
+    public GameObject CameraDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Up.transform.position.y <= CameraDown.transform.position.y)
+        {
+            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + (9990f*2), transform.position.z);
+        }
+    }
+}
